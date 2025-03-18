7082cc08a88529f3808c0dc60b175d17e777caf9
Jaxon Wright <jaxonhwright@gmail.com>
2025-03-17 21:03:07 -0400

    app: switch to scss, make navbar sticky

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { ThemeService } from './services/theme/theme.service';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
+    styleUrls: ['./app.component.scss'],
     standalone: false
 })
 export class AppComponent implements OnInit {
