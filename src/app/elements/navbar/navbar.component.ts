628c232d0e9eba358b81232653c8a224ef66eeb4
Jaxon Wright <jaxonhwright@gmail.com>
2025-03-17 21:58:48 -0400

    remove theming. black is the only acceptable theme.

--- src/app/elements/navbar/navbar.component.ts
+++ src/app/elements/navbar/navbar.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { AppComponent } from '@app/app.component';
-import { ThemeService, ThemeType } from '@services/theme/theme.service';
 import { MatToolbar, MatToolbarRow } from '@angular/material/toolbar';
 import { MatAnchor, MatIconAnchor, MatIconButton } from '@angular/material/button';
 import { MatTooltip } from '@angular/material/tooltip';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
-import { MatIcon } from '@angular/material/icon';
 import { LogoComponent } from "../logo/logo.component";
 
 @Component({
@@ -15,17 +13,13 @@ import { LogoComponent } from "../logo/logo.component";
     styleUrls: ['./navbar.component.scss'],
     imports: [
       MatToolbar, MatToolbarRow, MatAnchor, RouterLink, MatIconAnchor,
-      MatTooltip, FaIconComponent, MatIconButton,
-      MatIcon, RouterLinkActive, LogoComponent,
+      MatTooltip, FaIconComponent, RouterLinkActive, LogoComponent,
     ]
 })
 export class NavbarComponent {
-  ThemeType = ThemeType;
-  
   constructor(
     public router: Router,
     public app: AppComponent,
-    public theme: ThemeService
   ) { }
 
 }
